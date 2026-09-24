bd73e06579f81d6efb86c865957cbed09e95f8b7
adi-x69 <adityamaurya2020june2009@gmail.com>
2026-09-24 13:24:45 +0530

    update app/src/main/java/com/example/NetMirrorScreen.kt

--- app/src/main/java/com/example/NetMirrorScreen.kt
+++ app/src/main/java/com/example/NetMirrorScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.Modifier
+import androidx.compose.ui.modifier.modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
